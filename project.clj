2c49d73f64704960442e38e4cc5cb2f6c5ecf4e7
Jeff Hemminger <jeff@kropek.org>
2014-05-17 11:41:46 +1000

    added higher order functions, added dependency on numeric-tower

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clojure-koans "0.5.0-SNAPSHOT"
   :description "The Clojure koans."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [koan-engine "0.2.2"]]
+                 [koan-engine "0.2.2"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :dev-dependencies [[lein-koan "0.1.2"]]
   :profiles {:dev {:dependencies [[lein-koan "0.1.2"]]}}
   :repl-options {:init-ns koan-engine.runner
